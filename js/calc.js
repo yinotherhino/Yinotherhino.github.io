fb9815b35e6739b9016ebfbfc60cfd88566bf7b1
Yino IT <97189078+Yinotherhino@users.noreply.github.com>
2022-06-22 01:49:45 +0100

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -1,6 +1,6 @@
-num1= parseFloat(prompt("Enter your first number: \n<a href="https://github.com/Yinotherhino/jscalc">Repo link</a>"));
-operand= prompt("Enter your operand (only '+', '-', '*' and '/' allowed): \n<a href="https://github.com/Yinotherhino/jscalc">Repo link</a>");
-num2= parseFloat(prompt("Enter your second number: \n<a href="https://github.com/Yinotherhino/jscalc">Repo link</a>"));
+num1= parseFloat(prompt("Enter your first number: "));
+operand= prompt("Enter your operand (only '+', '-', '*' and '/' allowed): ">Repo link</a>");
+num2= parseFloat(prompt("Enter your second number: ">Repo link</a>"));
 
 switch (operand){
     case "+":
@@ -16,5 +16,5 @@ switch (operand){
         alert (num1 / num2);
         break;
     default:
-        alert ("Please only numbers are allowed for num1 and num2 and only these (+ - * /) operands are allowed \n <a href="https://github.com/Yinotherhino/jscalc">Repo link</a>");
+        alert ("Please only numbers are allowed for num1 and num2 and only these (+ - * /) operands are allowed ">Repo link</a>");
 }
